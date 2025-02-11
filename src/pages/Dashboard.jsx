c53918b2bdf57b737cca9d834338c004c955d835
Kasador <hunterstevenshaw@gmail.com>
2025-02-10 21:54:19 -0500

    Feat: Dashboard in progress. Added ChartJS library.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,61 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+// const data = {
+//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//     datasets: [{
+//         label: '# of Votes',
+//         data: [12, 19, 3, 5, 2, 3],
+//         borderWidth: 1
+//     }]
+// }
+new ChartJS(Doughnut, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+const doughnutData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'Friends Added',
+            backgroundColor: '#F09D51',
+            borderColor: '#F06543',
+            borderWidth: 1,
+            hoverBackgroundColor: '#F06543',
+            hoverBorderColor: '#F06543',
+            data: [7, 2, 12, 5, 17, 8, 4]
+        },
+    ], options: [ {
+        scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+    }]
+};
+
 const Dashboard = () => {
     return (
-        <h1>Dashboard</h1>
+        <div style={{width: '500px'}}>
+            <Doughnut data={doughnutData} options={doughnutData.options}/>
+        </div>
     )
 };
 
